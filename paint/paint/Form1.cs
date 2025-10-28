4c11a7958d54f90a2883e1e0f9463d815e3480e9
1107fan <1120427@stweb.jcjh.tp.edu.tw>
2025-10-28 15:39:50 +0800

    Add files via upload

---
+++ paint/paint/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 線條ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(800, 600);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+        Pen p = new Pen(Color.Black, 3);
+
+        int x, y;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void 藍ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void 黑ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Black;
+        }
+
+        private void 紅ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Red;
+        }
+
+        private void 細ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 1;
+        }
+
+        private void 中ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 3;
+        }
+
+        private void 粗ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.Width = 5;
+        }
+
+        private void 短虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+        }
+
+        private void 直線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+        }
+
+        private void 長虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+        }
+
+        private void 長短虛線ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+        }
+
+        private void 儲存檔案ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(@"C:\Users\user\Desktop\1.jpg", ImageFormat.Png);
+            MessageBox.Show("存檔成功");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(800, 600);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(pictureBox1.Image); // 指定picturebox為畫布
+                g.DrawLine(p, x, y, e.X, e.Y);
+
+                x = e.X;
+                y = e.Y;
+                pictureBox1.Refresh();
+
+            }
+        }
+    }
+}
